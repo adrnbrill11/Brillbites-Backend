1c131eb214e0fa4de82fb8bc875adc005a65287b
Your Name <adrian@Ians-MacBook-Air.local>
2026-06-08 17:54:58 +0800

    Remove dotenv

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+
 const app = express();
 const prisma = require("./config/prisma");
 const authRoutes = require("./routes/authRoutes");
