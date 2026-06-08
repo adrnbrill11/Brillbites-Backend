9e8135ebe8a0f0fb7bd3ea8dfcfb5249d160daef
Your Name <adrian@Ians-MacBook-Air.local>
2026-06-08 16:32:24 +0800

    Updated pg

--- config/prisma.js
+++ config/prisma.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require("@prisma/client")
+const { PrismaPg } = require("@prisma/adapter-pg")
 
-const prisma = new PrismaClient()
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
 
-module.exports = prisma
+const prisma = new PrismaClient({
+    adapter,
+})
+
+module.exports = prisma;
+
