c78606c981daf1b11520acdd571991626b038400
Your Name <adrian@Ians-MacBook-Air.local>
2026-06-18 14:07:18 +0800

    Add Inventory feautures

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+
 const prisma = require("./config/prisma");
 const authRoutes = require("./routes/authRoutes");
 const orderRoutes = require("./routes/orderRoutes")
